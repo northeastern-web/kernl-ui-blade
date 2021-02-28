11035590289faed0f5cead6824acd68069c19fa1
asantibanez <santibanez.andres@gmail.com>
2021-02-28 12:38:38 -0500

    Extracted views to partials
    - Added expandable and expanded modifiers

---
+++ src/views/includes/interior-navigation/base/link-with-children.blade.php
@@ -0,0 +1,57 @@
+<div
+    @if(data_get($link, 'expandable', false))
+        x-data="{
+            expanded: {{ data_get($link, 'expanded', false) ? 'true' : 'false' }}
+        }"
+    @endif
+>
+    <button
+        class="
+            w-full flex items-center justify-between text-left
+            @if(data_get($link, 'active', false))
+                px-2 border-l-2 transition-colors text-gray-900 font-bold border-red-600
+            @else
+                px-2 border-l-2 transition-colors border-transparent hover:text-gray-900
+            @endif
+        "
+        @if(data_get($link, 'expandable', false))
+            x-on:click="expanded = ! expanded"
+        @endif
+    >
+        <span>
+            {{ data_get($link, 'text', 'No text') }}
+        </span>
+        @if(data_get($link, 'expandable', false))
+            <svg
+                x-bind:class="{ 'rotate-180': expanded }"
+                class="ml-3 w-5 h-5 transform transition-transform rotate-180"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            >
+                <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+        @endif
+    </button>
+    <ul
+        @if(data_get($link, 'expandable', false))
+            x-show="expanded"
+            x-cloak
+        @endif
+        class="py-2 text-sm space-y-2"
+    >
+        @foreach(data_get($link, 'children', []) as $child)
+            <li>
+                <a
+                    href="{{ data_get($child, 'href', '#') }}"
+                    class="px-4 transition-colors hover:text-gray-900"
+                >
+                    {{ data_get($child, 'text', 'No text') }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
--- src/views/interior-navigation/base.blade.php
+++ src/views/interior-navigation/base.blade.php
@@ -67,56 +67,9 @@
         </a>
         @foreach($links as $link)
             @if(count(data_get($link, 'children', [])) > 0)
-                <div
-                    x-data="{ expanded: false }"
-                >
-                    <button
-                        class="w-full flex items-center justify-between px-2 text-left border-l-2 transition-colors border-transparent hover:text-gray-900"
-                        x-on:click="expanded = ! expanded"
-                    >
-                    <span>
-                        {{ data_get($link, 'text', 'No text') }}
-                    </span>
-                        <svg
-                            x-bind:class="{ 'rotate-180': expanded }"
-                            class="ml-3 w-5 h-5 transform transition-transform rotate-180"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        >
-                            <polyline points="6 9 12 15 18 9"></polyline>
-                        </svg>
-                    </button>
-                    <ul
-                        x-show="expanded"
-                        class="py-2 text-sm space-y-2"
-                    >
-                        @foreach(data_get($link, 'children', []) as $child)
-                            <li>
-                                <a
-                                    href="{{ data_get($child, 'href', '#') }}"
-                                    class="px-4 transition-colors hover:text-gray-900"
-                                >
-                                    {{ data_get($child, 'text', 'No text') }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                @include('kernl-ui::includes.interior-navigation.base.link-with-children', ['link' => $link])
             @else
-                <a
-                    href="{{ data_get($link, 'href', '#') }}"
-                    @if(data_get($link, 'active', false))
-                        class="px-2 border-l-2 transition-colors text-gray-900 font-bold border-red-600"
-                    @else
-                        class="mt-3 px-2 border-l-2 transition-colors border-transparent hover:text-gray-900"
-                    @endif
-                >
-                    {{ data_get($link, 'text', 'No text') }}
-                </a>
+                @include('kernl-ui::includes.interior-navigation.base.link', ['link' => $link])
             @endif
         @endforeach
     </nav>
