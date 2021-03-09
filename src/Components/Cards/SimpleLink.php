24942c1000d1c0e5f31a58739a76df9a5d63aaed
asantibanez <santibanez.andres@gmail.com>
2021-03-09 17:38:12 -0500

    Refactored implementation

--- src/Components/Cards/SimpleLink.php
+++ src/Components/Cards/SimpleLink.php
@@ -36,7 +36,7 @@ class SimpleLink extends Component
 
     public function cardClasses()
     {
-        $classes = collect()
+        return collect()
             ->merge([
                 'inline-flex',
                 'flex-col',
@@ -49,57 +49,59 @@ class SimpleLink extends Component
             ])
             ->merge([
                 $this->attributes->first('class'),
-            ]);
-
-        if ($this->color === 'light') {
-            $classes = $classes->merge(['text-black bg-white hover:bg-gray-100']);
-        }
-
-        if ($this->color === 'light-gray') {
-            $classes = $classes->merge(['text-black bg-gray-200 hover:bg-gray-300']);
-        }
-
-        if ($this->color === 'dark') {
-            $classes = $classes->merge(['text-white bg-black hover:bg-gray-800']);
-        }
-
-        if ($this->color === 'red') {
-            $classes = $classes->merge(['text-white bg-red-600 hover:bg-red-800']);
-        }
-
-        return $classes->join(' ');
+            ])
+            // Size
+            ->when($this->size === 'default', function ($classes) {
+                return $classes->push('max-w-xs', 'w-full', 'px-6', 'py-8');
+            })
+            ->when($this->size === 'sm', function ($classes) {
+                return $classes->push('w-64', 'p-4');
+            })
+            // Color
+            ->when($this->color === 'light', function ($classes) {
+                return $classes->push('text-black', 'bg-white', 'hover:bg-gray-100');
+            })
+            ->when($this->color === 'light-gray', function ($classes) {
+                return $classes->push('text-black', 'bg-gray-200', 'hover:bg-gray-300');
+            })
+            ->when($this->color === 'dark', function ($classes) {
+                return $classes->push('text-white', 'bg-black', 'hover:bg-gray-800');
+            })
+            ->when($this->color === 'red', function ($classes) {
+                return $classes->push('text-white', 'bg-red-600', 'hover:bg-red-800');
+            })
+            ->join(' ')
+            ;
     }
 
     public function titleClasses()
     {
-        $classes = collect();
-
-        if ($this->size === 'default') {
-            $classes = $classes->merge(['text-lg']);
-        }
-
-        if ($this->size === 'sm') {
-            $classes = $classes->merge(['text-sm']);
-        }
-
-        return $classes->join(' ');
+        return collect()
+            ->when($this->size === 'default', function ($classes) {
+                return $classes->merge(['text-lg']);
+            })
+            ->when($this->size === 'sm', function ($classes) {
+                return $classes->merge(['text-sm']);
+            })
+            ->join(' ')
+            ;
     }
 
     public function bodyClasses()
     {
-        $classes = collect([
-            'mt-2',
-        ]);
+        return collect()
+            ->merge([
+                'mt-2',
+            ])
+            ->when($this->size === 'default', function ($classes) {
+                return $classes->merge(['text-sm']);
+            })
 
-        if ($this->size === 'default') {
-            $classes = $classes->merge(['text-sm']);
-        }
-
-        if ($this->size === 'sm') {
-            $classes = $classes->merge(['text-xs']);
-        }
-
-        return $classes->join(' ');
+            ->when($this->size === 'sm', function ($classes) {
+                return $classes->merge(['text-xs']);
+            })
+            ->join(' ')
+            ;
     }
 
     public function render()
