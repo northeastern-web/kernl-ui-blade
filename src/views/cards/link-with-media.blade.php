8ebbfd1c0c779b7826b20be660678ddddfd7c9cb
Cameron Van Orman <cameron.vanorman@gmail.com>
2021-05-26 16:58:43 -0700

    Updating link-with-media-and-actions card by adjusting actions slot section where endif and div needed to be swapped

--- src/views/cards/link-with-media.blade.php
+++ src/views/cards/link-with-media.blade.php
@@ -1,24 +1,28 @@
 <a
-    href="{{ $url }}"
+    @if (!empty($url))
+        href="{{ $url }}"
+    @endif
     aria-label="{{ $title }}"
     class="{{ $cardClasses() }}"
 >
-    <div class="{{ $imageOuterImageContainerClasses() }}">
-        <div class="{{ $imageInnerImageContainerClasses() }}">
-            <img
-                src="{{ $imageUrl }}"
-                alt="{{ $title }}"
-                class="w-full h-full object-cover transition-opacity group-hover:opacity-80"
-            >
-            @if($badge)
-                <div class="absolute top-0 left-0 px-5 py-6">
-                    <span role="status" class="inline-flex px-4 py-2 text-xs text-white leading-none bg-black bg-opacity-50 rounded-md">
-                        {{ $badge }}
-                    </span>
-                </div>
-            @endif
+    @if ($imageUrl && !empty($imageUrl))
+        <div class="{{ $imageOuterImageContainerClasses() }}">
+            <div class="{{ $imageInnerImageContainerClasses() }}">
+                <img
+                    src="{{ $imageUrl }}"
+                    alt="{{ $title }}"
+                    class="w-full h-full object-cover transition-opacity group-hover:opacity-80"
+                >
+                @if($badge)
+                    <div class="absolute top-0 left-0 px-5 py-6">
+                        <span role="status" class="inline-flex px-4 py-2 text-xs text-white leading-none bg-black bg-opacity-50 rounded-md">
+                            {{ $badge }}
+                        </span>
+                    </div>
+                @endif
+            </div>
         </div>
-    </div>
+    @endif
     <div class="flex flex-col">
 
         <div class="{{ $bodyClasses() }}">
