9ba610ea756d8caf0cb1e54b9a2dcb5bd15250bc
Cameron Van Orman <cameron.vanorman@gmail.com>
2021-06-02 10:59:47 -0700

    Updating with-offset-card banner so that background does not have opacity added to it. (#19)

--- src/views/banners/with-offset-card.blade.php
+++ src/views/banners/with-offset-card.blade.php
@@ -1,9 +1,7 @@
 <div
     class="py-16 md:py-20 lg:py-32 bg-black bg-center bg-cover bg-no-repeat"
     {!! 
-        $backgroundUrl ? 'style="background-image: 
-        linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), 
-        url(' . $backgroundUrl . ')"' : ''
+        $backgroundUrl ? 'style="background-image: url(' . $backgroundUrl . ')"' : ''
     !!}
 >
     <div class="container">
