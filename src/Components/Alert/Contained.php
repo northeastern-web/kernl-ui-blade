af6e3a79a874e26581354457f9fc8ff3eba2f9f5
Cameron Van Orman <cameron.vanorman@gmail.com>
2021-06-04 14:48:25 -0700

    Updating alert views and component class

--- src/Components/Alert/Contained.php
+++ src/Components/Alert/Contained.php
@@ -3,18 +3,38 @@ namespace Northeastern\Blade\Components\Alert;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\View\Component;
+use InvalidArgumentException;
 
 class Contained extends Component
 {
     public $label;
     public $closeable;
     public $remember;
+    public $colorClasses;
 
-    public function __construct($label, $closeable = true, $remember = false)
+    protected $colors = [
+        'black' => 'text-black bg-white hover:text-white hover:bg-black',
+        'white' => 'text-white bg-black hover:text-black',
+        'gray-600' => 'text-gray-600 bg-white hover:text-white hover:bg-gray-600',
+        'gray-300' => 'text-gray-300 bg-gray-900 hover:text-gray-900 hover:bg-gray-300',
+        'red' => 'text-red-600 bg-white hover:text-white hover:bg-red-600',
+        'yellow' => 'text-black bg-yellow-400 hover:bg-black hover:text-yellow-400',
+        'blue' => 'text-blue-700 bg-blue-700 hover:text-white hover:bg-blue-700',
+        'green' => 'text-green-600 bg-green-600 hover:text-white hover:bg-green-600',
+        'aqua' => 'text-aqua-500 bg-aqua-500 hover:text-white hover:bg-aqua-500',
+    ];
+
+    public function __construct($label, $closeable = true, $remember = false, $color = 'yellow')
     {
         $this->label = $label;
         $this->closeable = $closeable;
         $this->remember = $remember;
+
+        if (isset($this->colors[$color])) {
+            $this->colorClasses = $this->colors[$color];
+        } else {
+            throw new InvalidArgumentException('`' . $color . '` is not a supported color option.');
+        }
     }
 
     public function render()
--- src/views/alert/full-width.blade.php
+++ src/views/alert/full-width.blade.php
@@ -34,14 +34,18 @@
         x-transition:leave="transition ease-in duration-200"
         x-transition:leave-end="transform opacity-0"
         role="alert"
-        class="bg-yellow-400"
+        {{ $attributes->merge(['class' => $colorClasses]) }}
     >
         <div class="flex items-center justify-between px-4 py-4 text-black lg:px-16">
             <div>
                 {{ $slot }}
             </div>
             @if ($closeable)
-                <button aria-label="close alert" class="ml-4 p-1 rounded-full transition-colors duration-200 hover:text-gray-800 focus:outline-none focus:ring focus:ring-blue-400" @click="close">
+                <button 
+                    aria-label="close alert" 
+                    class="ml-4 p-1 rounded-full transition-colors duration-200 hover:text-gray-800 focus:outline-none focus:ring focus:ring-blue-400" 
+                    @click="close"
+                >
                     <i data-feather="x-circle" class="w-5 h-5"></i>
                 </button>
             @endif
