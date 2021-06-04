0be7272cbe7fcdece19a08332c673b6ef37cca41
Cameron Van Orman <cameron.vanorman@gmail.com>
2021-06-04 15:27:08 -0700

    Updating contained color classes

--- src/Components/Alert/Contained.php
+++ src/Components/Alert/Contained.php
@@ -13,15 +13,9 @@ class Contained extends Component
     public $colorClasses;
 
     protected $colors = [
-        'black' => 'text-black bg-white hover:text-white hover:bg-black',
-        'white' => 'text-white bg-black hover:text-black',
-        'gray-600' => 'text-gray-600 bg-white hover:text-white hover:bg-gray-600',
-        'gray-300' => 'text-gray-300 bg-gray-900 hover:text-gray-900 hover:bg-gray-300',
-        'red' => 'text-red-600 bg-white hover:text-white hover:bg-red-600',
-        'yellow' => 'text-black bg-yellow-400 hover:bg-black hover:text-yellow-400',
-        'blue' => 'text-blue-700 bg-blue-700 hover:text-white hover:bg-blue-700',
-        'green' => 'text-green-600 bg-green-600 hover:text-white hover:bg-green-600',
-        'aqua' => 'text-aqua-500 bg-aqua-500 hover:text-white hover:bg-aqua-500',
+        'red' => 'text-white bg-red-600',
+        'yellow' => 'text-black bg-yellow-400',
+        'cool' => 'text-black bg-gray-cool-300',
     ];
 
     public function __construct($label, $closeable = true, $remember = false, $color = 'yellow')
