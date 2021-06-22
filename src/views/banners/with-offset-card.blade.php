ee9380fbeab2ee83f7da8261857949433f5b7560
Joel Kuehn <joel.kuehn@gmail.com>
2021-06-22 16:04:33 -0400

    Hero banner: split on responsive; first step

--- src/views/banners/with-offset-card.blade.php
+++ src/views/banners/with-offset-card.blade.php
@@ -4,9 +4,17 @@
         $backgroundUrl ? 'style="background-image: url(' . $backgroundUrl . ')"' : ''
     !!}
 >
-    <div class="container">
+    <img class="w-full object-cover sm:hidden" src="{{ $backgroundUrl }}" alt="Header image: {{ $backgroundUrl }}" />
+    <div class="container hidden sm:block">
         <div class="max-w-2xl px-6 py-12 text-white bg-black bg-opacity-80 lg:px-12 lg:py-16">
             {{ $slot }}
         </div>
     </div>
+
+    <div class="w-full bg-white block sm:hidden">   
+        <div class="container flex justify-start items-center h-full z-2">
+        {{ $slot }}
+        </div>
+    </div>
+
 </div>
