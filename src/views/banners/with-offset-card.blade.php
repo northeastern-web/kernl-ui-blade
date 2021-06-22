85d948d9fb59e427b6e63ef02ce6849201a8006b
Joel Kuehn <joel.kuehn@gmail.com>
2021-06-22 16:48:52 -0400

    Feature/modifyheaderbanner (#23)
    * Hero banner: split on responsive; first step
    
    * Modify header spacing and background

--- src/views/banners/with-offset-card.blade.php
+++ src/views/banners/with-offset-card.blade.php
@@ -1,12 +1,19 @@
 <div
-    class="py-16 md:py-20 lg:py-32 bg-black bg-center bg-cover bg-no-repeat"
+    class="py-24 md:py-20 lg:py-32 bg-black bg-center bg-cover bg-no-repeat"
     {!! 
         $backgroundUrl ? 'style="background-image: url(' . $backgroundUrl . ')"' : ''
     !!}
 >
-    <div class="container">
+    <div class="container hidden sm:block">
         <div class="max-w-2xl px-6 py-12 text-white bg-black bg-opacity-80 lg:px-12 lg:py-16">
             {{ $slot }}
         </div>
+    </div>
+</div>
+
+
+<div class="py-8 w-full bg-gray-100 block sm:hidden">   
+    <div class="container h-full z-2">
+    {{ $slot }}
     </div>
 </div>
