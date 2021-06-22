104ea25f25c50d2a84caf75932df7b045e550507
Joel Kuehn <joel.kuehn@gmail.com>
2021-06-22 16:42:21 -0400

    Modify header spacing and background

--- src/views/banners/with-offset-card.blade.php
+++ src/views/banners/with-offset-card.blade.php
@@ -1,20 +1,19 @@
 <div
-    class="py-16 md:py-20 lg:py-32 bg-black bg-center bg-cover bg-no-repeat"
+    class="py-24 md:py-20 lg:py-32 bg-black bg-center bg-cover bg-no-repeat"
     {!! 
         $backgroundUrl ? 'style="background-image: url(' . $backgroundUrl . ')"' : ''
     !!}
 >
-    <img class="w-full object-cover sm:hidden" src="{{ $backgroundUrl }}" alt="Header image: {{ $backgroundUrl }}" />
     <div class="container hidden sm:block">
         <div class="max-w-2xl px-6 py-12 text-white bg-black bg-opacity-80 lg:px-12 lg:py-16">
             {{ $slot }}
         </div>
     </div>
+</div>
 
-    <div class="w-full bg-white block sm:hidden">   
-        <div class="container flex justify-start items-center h-full z-2">
-        {{ $slot }}
-        </div>
+
+<div class="py-8 w-full bg-gray-100 block sm:hidden">   
+    <div class="container h-full z-2">
+    {{ $slot }}
     </div>
-
 </div>
