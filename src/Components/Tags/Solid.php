b88d762480e93e96fcea84cb0a74280d1277c9ca
Cameron Van Orman <cameron.vanorman@gmail.com>
2022-04-04 23:47:51 -0700

    Minor Fixes (#41)
    * Update tag component swatch values. Update README
    
    * Fix styling
    
    * Update default color for tags.solid
    
    Co-authored-by: cam-vanorman <cam-vanorman@users.noreply.github.com>

--- src/Components/Tags/Solid.php
+++ src/Components/Tags/Solid.php
@@ -24,7 +24,7 @@ class Solid extends Component
         'yellow' => ['text-black', 'bg-yellow-300'],
     ];
 
-    public function __construct($color = 'black', $pill = false, $uppercase = false)
+    public function __construct($color = 'dark', $pill = false, $uppercase = false)
     {
         if (! array_key_exists($color, $this->colors)) {
             throw new InvalidArgumentException('`' . $color . '` is not a supported color option.');
